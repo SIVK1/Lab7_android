e4df968f20910defa4da1e2ce4d82a5260c024c8
Artem Svk <artems2715@gmail.com>
2024-03-25 22:01:38 +0300

    NotesScreen.kt  complete

--- app/src/main/java/com/topic2/android/notes/screens/NotesScreen.kt
+++ app/src/main/java/com/topic2/android/notes/screens/NotesScreen.kt
@@ -1,102 +1,39 @@
 package com.topic2.android.notes.screens
 
-import android.annotation.SuppressLint
+
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.FabPosition
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.List
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.tooling.preview.Preview
 import com.topic2.android.notes.domain.model.NoteModel
 import com.topic2.android.notes.viewmodel.MainViewModel
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
-import com.topic2.android.notes.routing.Screen
-import kotlinx.coroutines.launch
-import ui.components.AppDrawer
 import ui.components.Note
+import ui.components.TopAppBar
 
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun NotesScreen(viewModel: MainViewModel) {
+fun NotesScreen(viewModel: MainViewModel){
 
     val notes: List<NoteModel> by viewModel
         .notesNotInTrash
         .observeAsState(listOf())
 
-    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    Column {
+        TopAppBar(
+            title = "Заметки",
+            icon = Icons.Filled.List,
+            onIconClick = {}
+        )
+        NotesList(
+            notes = notes,
+            onNoteCheckedChange = {viewModel.onNoteCheckedChange(it)},
+            onNoteClick = {viewModel.onNoteClick(it)})
+    }
 
-    val coroutineScope = rememberCoroutineScope()
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "Notes",
-                        color = MaterialTheme.colors.onPrimary
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        coroutineScope.launch {
-                            scaffoldState.drawerState.open()
-                        }
-                    }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.List,
-                            contentDescription = "Drawer Button"
-                        )
-                    }
-                }
-            )
-        },
-        floatingActionButtonPosition = FabPosition.End,
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { viewModel.onCreateNewNoteClick()},
-                contentColor = MaterialTheme.colors.background,
-                content = {
-                    Icon(
-                        imageVector = Icons.Filled.Add,
-                        contentDescription = "Add Note Button"
-                    )
-                }
-            )
-        },
-
-        scaffoldState = scaffoldState,
-        drawerContent = {
-            AppDrawer(currentScreen = Screen.Notes, closeDrawerAction = {
-                coroutineScope.launch {
-                    scaffoldState.drawerState.close()
-                }
-            }
-            )
-        },
-        content = {
-            if (notes.isNotEmpty()) {
-                NotesList(
-                    notes = notes,
-                    onNoteCheckedChange = { viewModel.onNoteCheckedChange(it) },
-                    onNoteClick = { viewModel.onNoteClick(it) }
-                )
-            }
-        }
-    )
 }
 
 @Composable
