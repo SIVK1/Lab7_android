1788ff5b068b31159c7bb5177864531a2f7ac761
Artem Svk <artems2715@gmail.com>
2024-03-25 22:01:23 +0300

    NoteColor.kt  complete

--- app/src/main/java/ui/components/NoteColor.kt
+++ app/src/main/java/ui/components/NoteColor.kt
@@ -1,13 +1,13 @@
 package ui.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.runtime.Composable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -21,29 +21,25 @@ fun NoteColor(
     modifier: Modifier = Modifier,
     color: Color,
     size: Dp,
-    padding: Dp = 0.dp,
     border: Dp
-){
+) {
     Box(
         modifier = modifier
-            .padding(padding)
             .size(size)
             .clip(CircleShape)
             .background(color)
             .border(
                 BorderStroke(
-                    2.dp,
+                    border,
                     SolidColor(Color.Black)
-
                 ),
                 CircleShape
             )
-
     )
 }
 
 @Preview
 @Composable
-fun NoteColorPreview(){
-    NoteColor(color = Color.Red, size = 40.dp, padding = 4.dp, border = 2.dp)
+fun NoteColorPreview() {
+    NoteColor(color = Color.Red, size = 40.dp, border = 2.dp)
 }
