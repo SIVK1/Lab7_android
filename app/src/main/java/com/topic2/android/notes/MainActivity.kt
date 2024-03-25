d1904c68cea01b97b5379372ba821177e7eab719
Artem Svk <artems2715@gmail.com>
2024-03-25 22:00:40 +0300

    MainActivity.kt  complete

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -5,14 +5,15 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
-import com.topic2.android.notes.viewmodel.MainViewModel
-import com.topic2.android.notes.viewmodel.MainViewModelFactory
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.rememberCoroutineScope
 import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.screens.NotesScreen
 import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.viewmodel.MainViewModel
+import com.topic2.android.notes.viewmodel.MainViewModelFactory
 import kotlinx.coroutines.launch
 import ui.components.AppDrawer
 import ui.components.Note
@@ -28,35 +29,13 @@ class MainActivity : AppCompatActivity() {
       (application as NotesApplication).dependencyInjector.repository
     )
   })
-
   @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
       NotesTheme {
-        val coroutineScope = rememberCoroutineScope()
-        val scaffoldState: ScaffoldState = rememberScaffoldState()
-
-        Scaffold(
-          scaffoldState = scaffoldState,
-          drawerContent = {
-            AppDrawer(
-              currentScreen = Screen.Notes,
-              closeDrawerAction = {
-                coroutineScope.launch {
-                  scaffoldState.drawerState.close()
-                }
-              }
-            )
-
-          },
-          content = {
-            Note()
-          }
-        )
-
-
+        NotesScreen(viewModel = viewModel)
       }
     }
   }
